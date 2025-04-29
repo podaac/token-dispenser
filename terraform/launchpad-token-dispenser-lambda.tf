f5ab11f717a9a063fbbfc5c3dfa84f684b863e22
David Yen <david.yen@jpl.nasa.gov>
2025-04-29 12:48:07 -0700

    direct reference the cloudtrail s3 bucket arn.

--- terraform/launchpad-token-dispenser-lambda.tf
+++ terraform/launchpad-token-dispenser-lambda.tf
@@ -145,22 +145,22 @@ resource "aws_s3_bucket_policy" "tds_cloudtrail_bucket_policy" {
     Version = "2012-10-17",
     Statement = [
       {
-        Sid: "AWSCloudTrailWrite20150309",
+        Sid = "TDSCloudTrailGetBucketAcl",
         Effect = "Allow",
         Principal = {
           Service = "cloudtrail.amazonaws.com"
         },
         Action = "s3:GetBucketAcl",
-        Resource = "arn:aws:s3:::${aws_s3_bucket.cloudtrail_bucket.id}"
+        Resource = "${aws_s3_bucket.cloudtrail_bucket.arn}"
       },
       {
-        Sid: "AWSCloudTrailWrite",
+        Sid = "TDSCloudTrailWrite",
         Effect = "Allow",
         Principal = {
           Service = "cloudtrail.amazonaws.com"
         },
         Action = "s3:PutObject",
-        Resource = "arn:aws:s3:::${aws_s3_bucket.cloudtrail_bucket.id}/AWSLogs/${data.aws_caller_identity.current.account_id}/*",
+        Resource = "${aws_s3_bucket.cloudtrail_bucket.arn}/AWSLogs/${data.aws_caller_identity.current.account_id}/*",
         Condition = {
           StringEquals = {
             "s3:x-amz-acl" = "bucket-owner-full-control"
